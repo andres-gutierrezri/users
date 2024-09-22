58b93f9b4748ee5442ed29fef7de25a5e2d4e679
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-21 22:51:55 -0500

    USUARIOS-develop feat: Se modifica la configuración del proyecto para realizar el despliegue de la aplicación en el Hosting seleccionado.
    - Se modifica la configuración del proyecto en el archivo "settings.py" para realizar el despliegue de la aplicación en el Hosting seleccionado.

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -10,11 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
+# Importar os para manejar las variables de entorno
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from dotenv import load_dotenv
+from djangocrud.local_settings import IS_DEPLOYED, DATABASE_DICT
+from djangocrud.logging_settings import *
+from djangocrud.cloud_settings import *
+
+# Carga las variables de entorno del archivo .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Construir rutas dentro del proyecto de esta manera: BASE_DIR / 'subdir'.
+# from pathlib import Path
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -23,12 +35,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(oa(omhdw75#3qzk_p-6zfdfmvj#%tn=oci!ww+ssog(ib%-o='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Seguridad: no ejecute con depuración activada en producción!
+# Se recomienda que DEBUG sea False en producción
+# Se recomienda que DEBUG sea True en desarrollo
+DEBUG = not IS_DEPLOYED
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'usuarios-tasks.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://usuarios-tasks.up.railway.app']  
 
 # Application definition
+# Definición de aplicaciones
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,9 +54,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'whitenoise.runserver_nostatic',
+    'storages',
     'tasks'
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,10 +69,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+
+# URL Configuration
+# Configuración de URL
+# https://docs.djangoproject.com/en/4.1/topics/http/urls/
 ROOT_URLCONF = 'djangocrud.urls'
 
+# Template Configuration
+# Configuración de plantillas
+# https://docs.djangoproject.com/en/4.1/topics/templates/
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -68,21 +97,17 @@ TEMPLATES = [
     },
 ]
 
+# WSGI Configuration
+# Configuración de WSGI
 WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 
 # Database
+# Configuración de la base de datos
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'database.cnf',
-            'sql_mode': 'traditional',
-            'init_command': 'SET default_storage_engine=INNODB',
-        },
-    }
+    'default': DATABASE_DICT
 }
 
 
@@ -111,20 +136,47 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'America/Bogota'
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-LOGIN_URL = '/signin'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# Archivos estáticos (CSS, JavaScript, imágenes)
+# Configura la ruta de los archivos estáticos
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = '/staticfiles/' if IS_DEPLOYED else '/static/'
+
+LOGIN_URL = '/signin'
+
+"""
+# Configuración para almacenar archivos estáticos en S3
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+"""
+
+# Configuración para almacenar archivos multimedia en el sistema de archivos (S3)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not IS_DEPLOYED:
+    MEDIA_URL = '/media/'
+else:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    
+# Se define el nombre de la carpeta de archivos públicos para almacenar las imágenes de las caratulas de los libros
+PUBLIC_MEDIA = 'publico'
